b3896e597fa34dfbb723abf80f79d17aacc0335a
Gabriel Creti <gabrielcreti@gmail.com>
2022-11-14 17:08:00 +0100

    Add __init__ to module

---
+++ gitlab_siphon/__init__.py
@@ -0,0 +1,14 @@
+__title__ = "Gitlab Siphon"
+__version__ = "0.1.0"
+__author__ = "Gabriel Creti"
+__license__ = "Apache"
+__copywrite__ = "Copyright (C) 2022 Gabriel Creti <gabrielcreti@gmail.com>"
+__email__ = "gabrielcreti@gmail.com"
+__status__ = "Development"
+
+from __main__ import siphon
+
+
+__all__ = [
+    "siphon",
+]
